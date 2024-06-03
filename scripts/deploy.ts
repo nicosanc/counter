ba3e0d2a9fa7af0ba06fd74b1f8e6a7e84503b16
Nico Sanchez <nicosanchez0411@gmail.clm>
2024-06-03 17:16:56 -0400

    pushing to my own forked repo

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -28,9 +28,8 @@ async function main() {
 
   const myCallData = new CallData(sierraCode.abi);
   const constructor = myCallData.compile("constructor", {
-    initial_counter: 100,
-    address:
-      "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
+    value: 100,
+    address: "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
     initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
   });
   const deployResponse = await account0.declareAndDeploy({
